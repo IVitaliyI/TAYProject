56f678138cf2e5dd92574d9834226e7bcb0fc0a5
Davydov Vitaliy <ychebn1y@yandex.ru>
2023-12-03 15:27:32 +0300

    Create a constants file
    Создал файл всех констант. Удалил все маски. Система начала выдавать результаты

--- constants.m
+++ constants.m
@@ -1,6 +1,4 @@
-%% Constants
-
-
+% /////// CONSTANTS \\\\\\\\
 m = .1;
 M = .2;
 L = .5;
@@ -8,4 +6,50 @@ g = 9.8;
 
 ts = 3/7;
 
+
+%% Input data
+angle = 0;
+vel = 0;
+
+
+%% High level controller
+k_i = 1;
+k_p = 1;
+k_d = 1;
+
+
+%% Low level controller
+parallel_correct = 1;
+
+
+%% Curent sensor
+R_cur = 0.01;
+
+
+%% Power gain
+gain_pw = 24 / 3.3;
+max_output_voltage = 24;
+
+%% DC motor
+L = 350 * 10^-6;
+R_dc = 10;
+kw = 400 * 60 / (2 * pi);
+km = 16;
+J_motor = 500 * 0.5 * 10^-6;
+vis_friction_coef_motor = 0.05;
+
+
+%% Gearbox
+i_gear = 100;
+J_gear = 2.4 * 10^-6;
+vis_friction_coef_gear = 0.05;
+
+%% Rail gearbox
+i_rail = 20;
+k_visco = 0.05;
+m_rail = 0.5;
+J_rail = 1 * 10^-6;
+
 %%
+
+% /////// CONSTANTS \\\\\\\\
